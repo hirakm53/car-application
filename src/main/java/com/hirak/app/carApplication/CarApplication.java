5d7ecffa5996a506197b8b2832e4bd28340b497f
Hirak Jyoti Mahanta <hirak.mahanta@bounceshare.com>
2020-04-26 14:11:48 +0530

    Added db properties

---
+++ src/main/java/com/hirak/app/carApplication/CarApplication.java
@@ -0,0 +1,17 @@
+package com.hirak.app.carApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hirak_mahanta
+ *
+ */
+@SpringBootApplication
+public class CarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarApplication.class, args);
+	}
+
+}
